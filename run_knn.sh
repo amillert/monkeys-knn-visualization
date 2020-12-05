db0072c3a0261563af3dcdad056fa91ff274f1e0
amillert <albertmillert97@gmail.com>
2020-12-05 20:00:15 +0100

    Updated running shell scripts

--- run_knn.sh
+++ run_knn.sh
@@ -1,1 +1,5 @@
-python -m unittest -v tests.py & ./monkey_classify.py knn -i monkeys.csv -o out.csv -d bmi fur_color_int weight
+if [ $1 ]; then
+  echo 1; ./monkey_classify.py knn -i monkeys.csv -o $1 -d bmi fur_color_int weight
+else
+  echo 2; python -m unittest -v tests.py; ./monkey_classify.py knn -i monkeys.csv -o out.csv -d bmi fur_color_int weight
+fi
--- run_visualize.sh
+++ run_visualize.sh
@@ -1,1 +1,9 @@
-python -m unittest -v tests.py & ./monkey_classify.py visual -i out.csv -f weight fur_color_int
+if [ $1 ]; then
+  if [ -e $1 ]; then
+    echo 1 && python -m unittest -v tests.py && ./monkey_classify.py visual -i $1 -f weight fur_color_int
+  else
+    echo 2 && python -m unittest -v tests.py && ./run_knn.sh $1 && ./monkey_classify.py visual -i $1 -f weight fur_color_int
+  fi
+else
+  echo 3 && python -m unittest -v tests.py && ./run_knn.sh out.csv && ./monkey_classify.py visual -i out.csv -f weight fur_color_int
+fi
