06bd539bb55a031bc4c04bb84c569596f1e4998a
amillert <albertmillert97@gmail.com>
2020-12-09 11:30:44 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='monkeys-knn-visualization',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='amillert',
+    author_email='devmood.business@gmail.com',
+    description=''
+)
