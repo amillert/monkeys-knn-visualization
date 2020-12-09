177c43b5c0a90cc5ed98ec9a18917a09ce9c285c
amillert <albertmillert97@gmail.com>
2020-12-09 11:31:26 +0100

    Updated running shell scripts

--- run_knn.sh
+++ run_knn.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+set -e
+
 if [ $1 ]; then
-  echo 1; ./monkey_classify.py knn -i monkeys.csv -o $1 -d bmi fur_color_int weight
+  echo 1
+  ./monkey_classify.py knn -i monkeys.csv -o $1 -d bmi fur_color_int weight
 else
-  echo 2; python -m unittest -v tests.py; ./monkey_classify.py knn -i monkeys.csv -o out.csv -d bmi fur_color_int weight
+  echo 2
+  python -m unittest -v tests.py
+  ./monkey_classify.py knn -i monkeys.csv -o out.csv -d bmi fur_color_int weight
 fi
--- run_visualize.sh
+++ run_visualize.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+set -e
+
 if [ $1 ]; then
   if [ -e $1 ]; then
-    echo 1 && python -m unittest -v tests.py && ./monkey_classify.py visual -i $1 -f weight fur_color_int
+    echo 1
+    python -m unittest -v tests.py
+    ./monkey_classify.py visual -i $1 -f weight fur_color_int
   else
-    echo 2 && python -m unittest -v tests.py && ./run_knn.sh $1 && ./monkey_classify.py visual -i $1 -f weight fur_color_int
+    echo 2
+    python -m unittest -v tests.py
+    ./run_knn.sh $1 && ./monkey_classify.py visual -i $1 -f weight fur_color_int
   fi
 else
-  echo 3 && python -m unittest -v tests.py && ./run_knn.sh out.csv && ./monkey_classify.py visual -i out.csv -f weight fur_color_int
+  echo 3
+  python -m unittest -v tests.py
+  ./run_knn.sh out.csv && ./monkey_classify.py visual -i out.csv -f weight fur_color_int
 fi
